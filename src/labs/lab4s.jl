9045cff51cf59bbee8f1932d3f16140c0d387b2f
Sheehan Olver <solver@mac.com>
2024-03-19 21:08:19 +0000

    DiffEq autodiff example

--- src/labs/lab4s.jl
+++ src/labs/lab4s.jl
@@ -1,2 +1,51 @@
 # # SciML SANUM2024
 # # Lab 4: Solving differential equations in DifferentialEquations.jl
+
+
+using DifferentialEquations, Plots
+
+# ## DifferentialEquations.jl
+
+# Consider the pendulum equation with friction
+# $$
+# u'' = τ*u' - \sin u
+# $$
+# which we can rewrite as a first order system:
+# $$
+# [u᾽,v᾽] = [v, -τ*v - sin(u)]
+# $$
+# This is translated to an ODE as follows:
+
+
+function pendulum(du, u, τ, t)
+    du[1] = u[2]
+    du[2] = -sin(u[1]) - τ*u[2]
+end
+
+τ = 0.0 # no friction
+T = 10.0 # final time
+u₀, v₀ = 1,1
+prob = ODEProblem(pendulum, [u₀, v₀], (0.0, T), τ)
+sol = solve(prob, Vern9(), abstol = 1e-10, reltol = 1e-10)
+
+plot(sol)
+
+
+
+
+# ## Combining auto-differentiation with DifferentialEquations.jl
+
+
+using ForwardDiff
+using ForwardDiff: derivative, gradient
+
+function pendulumfriction(τ)
+    T = 10.0 # final time
+    u₀, v₀ = 1,1
+    prob = ODEProblem(pendulum, [u₀, v₀], (0.0, T), τ)
+    sol = solve(prob, Vern9(), abstol = 1e-10, reltol = 1e-10)
+    sol[end]
+end
+
+
+derivative(pendulumfriction, 1.0)
