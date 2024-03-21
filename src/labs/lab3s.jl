6d44b0795b5cf1ce032c658b80b3cdffb9f72272
Sheehan Olver <solver@mac.com>
2024-03-21 14:55:16 +0000

    Work on reverse mode autodiff

--- src/labs/lab3s.jl
+++ src/labs/lab3s.jl
@@ -9,9 +9,9 @@
 
 # **Learning Outcomes**
 #
-# 1. Basics of reverse-mode automatic differentiation, "adjoint"s and back-propagation.
+# 1. Basics of reverse-mode automatic differentiation and pullbacks.
 # 2. Implementation via Zygote.jl
-# 3. Adding custom "adjoint"s. 
+# 3. Adding custom pullbacks.
 
 
 
@@ -25,10 +25,137 @@ f = function(x)
     ret
 end
 
+f = x -> sum(x[1:end-1] .* x[2:end])
+
+f = x -> sum(sin, x)
+
 x = randn(5)
 Zygote.gradient(f,x)
 
 # The one catch is the complexity is quadratic:
+Base.GC.enable(false)
+n = 100_000_000; @time Zygote.gradient(f,randn(n));
 
-@time Zygote.gradient(f,randn(1000));
+Base.GC.enable(true)
+Base.GC.gc()
 @time Zygote.gradient(f,randn(10_000)); # around 100x slower
+
+using ForwardDiff
+@time ForwardDiff.gradient(f,randn(10_000)); # around 100x slower
+
+# Under the scenes this determines so-called "pullback"s. In the scalar
+# case these are very close to the notion of a derivative. However, rather than
+# the derivative being a single constant, its a linear map: eg, if the derivative
+# of $f(x)$ is denoted $f'(x)$ then the pullback is a linear map
+# $$
+# t ↦ tf'(x).
+# $$
+# In the scalar case this can also be written in a more natural $f'(x)t$ form
+# but the order will become important when we discuss non-scalar functions. 
+# We can compute pullbacks using the `pullback` routine:
+
+import Zygote: pullback
+
+s, sin_J = pullback(sin, 0.1)
+
+# `sin_J` contains the map $t ↦ t \cos 0.1$. Since pullbacks support multiple arguments
+# it actually returns a tuple with a single entry:
+
+sin_J(1)
+
+# Thus to get out the value we use the following:
+
+@test sin_J(1)[1] == cos(0.1)
+@test sin_J(2)[1] == 2cos(0.1)
+
+# The reason its a map instead of a function becomes important for composing functions. Lets consider
+# a composition of three functions:
+# $$
+# f(g(h(x)) = f'(g(h(x)) g'(h(x)) h'(x)
+# $$
+# Essentially we have three pullbacks: the first is the pullback of $h$ evaluated
+# at $x$, the second corresponding to $g$ evaluated at $h(x)$, and the third 
+# corresponding to $f$ evaluated at $g(h(x))$, that is:
+# $$
+# \begin{align*}
+#  p_1(t) &= t h'(x) \\
+#  p_2(t) &= t g'(h(x)) \\
+#  p_3(t) &= t f'(g(h(x))
+# \end{align*}
+# $$
+# Thus the derivative is given by the reverse composition:
+# $$
+#  p_1(p_2(p_3(1)))
+# $$
+# In the scalar case, these are linear and commuting maps so you might wonder
+# why not use an arguably more natural $p_3(p_2(p_1(1))$, but the reverse order
+# becomes essential in higher dimensions.
+
+# Let's see this in action for computing the derivative of $\cos\sqrt{{\rm e}^x}$:
+
+x = 0.1 # point we want to differentiate
+y,p₁ = pullback(exp, x) 
+z,p₂ = pullback(sqrt, y) # y is exp(x)
+w,p₃ = pullback(cos, z) # z is sqrt(exp(x))
+
+@test w == cos(sqrt(exp(x)))
+
+@test p₁(p₂(p₃(1)...)...)[1] ≈ -sin(sqrt(exp(x)))*exp(x)/(2sqrt(exp(x)))
+
+
+# We can see how this can lead to an approach for automatic differentiation.
+# For example,   consider the following function composing `sin`` over and over:
+
+function manysin(n, x)
+    r = x
+    for k = 1:n
+        r = sin(r)
+    end
+    r
+end
+
+# Now, we would need `n` pullbacks as each time `sin` is called at a different value.
+# But the number of such pullbacks grows only linearly so this is acceptable. So thus
+# at a high-level we can think of Zygote as running through and computing all the pullbacks:
+
+n = 5
+x = 0.1 # input
+
+pullbacks = Any[] # a vector where we store the pull backs
+r = x
+for k = 1:n
+    r,pₖ = pullback(sin, r) # new pullback
+    push!(pullbacks, pₖ)
+end
+r # value
+
+# To deduce the derivative we need to do "back-propogation": loop through our pullbacks
+# in reverse:
+
+der = 1 # we always initialise with the trivial scaling
+for k = n:-1:1
+    der = pullbacks[k](der)[1]
+end
+@test der ≈ ForwardDiff.derivative(x -> manysin(n, x), x)
+
+# Zygote constructs code that is equivalent to this loop automatically, and without the need for creating a "tape",
+# that is, it doesn't actually allocate a vector `pullbacks` to record the operations at run-time, rather,
+# deduces a high-performance version of this back-propogation loop at compile time.
+
+# **Problem** Write a simple function for Taylor series of exponential. 
+# Write a function that implements back-propogation
+
+# ## Gradients
+#
+# Above we introduced  scalar. Now we consider computing gradients, which is
+# essential in ML. 
+
+z,pb = rrule(sum, [1, 2])
+
+Zygote.jacobian(x -> broadcast(sin, x), [1,2])[1]
+rrule(broadcast, sin, [1,2])
+
+unthunk(pb(1)[2])
+
+@ent Zygote.gradient(sum, [1,2])
+
